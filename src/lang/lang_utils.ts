38a7ab27af0492e7e690147aeb3a5e05e25d3d00
fbn776 <fbn776@gmail.com>
2024-01-25 23:33:47 +0530

    Feature: display format for AST

--- src/lang/lang_utils.ts
+++ src/lang/lang_utils.ts
@@ -1,7 +1,23 @@
+import CommandToken from "./tokens/CommandToken";
+import LangTokenBase from "./tokens/LangTokenBase";
+
 /** Checks if a string only contains unique characters (may/may not repeat)*/
 export function hasOnlyRepeatedChars(str: string) {
-    for(let a of str) {
-        if(a !== str[0]) return false;
+    for (let a of str) {
+        if (a !== str[0]) return false;
     }
     return true;
+}
+
+/** Displays a syntax tree and also does the formatting*/
+export function displayAST(base: LangTokenBase, level: number = 0, sep: string  = "  ") {
+    if (base instanceof CommandToken) {
+        console.log(`${sep.repeat(level)}>`, base.commandName);
+
+        for (let i of base.params) {
+            displayAST(i, level + 1, sep);
+        }
+    } else {
+        console.log(`${sep.repeat(level)}|`, base.value);
+    }
 }
