0cab89468c7506deae7d5506c3ab0301f662e3a1
fbn776 <fbn776@gmail.com>
2024-02-21 00:41:52 +0530

    Testing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import ASLangError from "./lang-core/errors/ASLangError";
-import {parser} from "./lang-core/interpreter/parser/parser";
 import CommandStore from "./lang-core/interpreter/CommandStore";
 import {CommandBuilder} from "./lang-core/specs/CommandBuilder";
 import {DocsBuilder} from "./lang-core/specs/DocsBuilder";
-import Arguments from "./lang-core/specs/lang-units/Arguments";
-import DataTypes from "./lang-core/specs/tokens/DataType";
+import Parameters from "./lang-core/specs/lang-units/Parameters";
+import DataType from "./lang-core/specs/tokens/DataType";
 import Runner from "./lang-core/interpreter/runner/Runner";
 
 function test(func: () => any) {
@@ -18,17 +17,17 @@ function test(func: () => any) {
     }
 }
 
-test(() => {
-    console.log(parser("hello jjs'haha' 2 3"))
-});
-
-test(() => {
-    console.log(parser("hello (('haha') 2 3"))
-});
-
-test(() => {
-    console.log(parser("add 10 43 24.234 34+13 true (sub -422 -42.44 +23 false) False True"))
-});
+// test(() => {
+//     console.log(parser("hello jjs'haha' 2 3"))
+// });
+//
+// test(() => {
+//     console.log(parser("hello (('haha') 2 3"))
+// });
+//
+// test(() => {
+//     console.log(parser("add 10 43 24.234 34+13 true (sub -422 -42.44 +23 false) False True"))
+// });
 
 
 
@@ -46,8 +45,8 @@ store.addCommand(
             .example('')
             .build()
         )
-        .args(new Arguments(DataTypes.number, DataTypes.number))
-        .returnType(DataTypes.number)
+        .args(new Parameters(2, DataType.number, DataType.number))
+        .returnType(DataType.number)
         .run((x: number, y: number) => {
             console.log(x, y);
             return x + y;
@@ -57,4 +56,4 @@ store.addCommand(
 
 const exec = new Runner();
 
-console.log(exec.execute('add 10 20'));
+console.log(exec.run('add true false'));
