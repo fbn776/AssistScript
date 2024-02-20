5fc2a6a6cc366c7eed4d855e4a5712b65ba73c3d
fbn776 <fbn776@gmail.com>
2024-02-21 00:41:30 +0530

    feat: Implemented rules for parameter checking

--- src/lang-core/interpreter/runner/Runner.ts
+++ src/lang-core/interpreter/runner/Runner.ts
@@ -1,33 +1,77 @@
 import CommandStore from "../CommandStore";
 import generateSyntaxTree from "../parser/generateSyntaxTree";
 import CommandToken from "../../specs/tokens/lexmes/CommandToken";
-import NumberToken from "../../specs/tokens/lexmes/NumberToken";
 import ASRuntimeError from "../../errors/ASRuntimeError";
+import LangTokenBase from "../../specs/tokens/LangTokenBase";
+import DataType from "../../specs/tokens/DataType";
+import NumberToken from "../../specs/tokens/lexmes/NumberToken";
+import BooleanToken from "../../specs/tokens/lexmes/BooleanToken";
+import StringToken from "../../specs/tokens/lexmes/StringToken";
+import Parameters from "../../specs/lang-units/Parameters";
 
 export default class Runner {
     private _store = CommandStore.getInstance();
 
-    public execute(str: string) {
+    public run(str: string) {
         const ast = generateSyntaxTree(str);
-        return executeCmd(this._store, ast);
+        return runCommand(ast, this._store);
     }
 }
 
-function executeCmd(store: CommandStore, cmd: CommandToken): any {
-    const cmdUnit = store.getCommand(cmd.commandName);
-    if (!cmdUnit)
-        throw new ASRuntimeError(`Command '${cmd.commandName}' not found.`);
+function runCommand(commandToken: CommandToken, store: CommandStore, ): unknown {
+    const commandDef = store.getCommand(commandToken.commandName);
+    if (!commandDef)
+        throw new ASRuntimeError(`Command '${commandToken.commandName}' not found.`);
 
-    let params = cmd.params;
+    const tokenParams = commandToken.params;
+    const commandParams = commandDef.params;
 
-    const paramsCP = params.map((value) => {
-        if (value instanceof CommandToken)
-            return executeCmd(store, value);
-        else if (value instanceof NumberToken)
-            return +(value.value as number);
-        else
-            return value.value as string;
+    // TODO Make this a proper error;
+    if(!commandParams.isVariable && tokenParams.length < commandParams.num) {
+        throw new ASRuntimeError(`The command expects ${commandParams.num} arguments, but found ${tokenParams.length}`);
+    }
+
+    const paramsCP = tokenParams.map((token, index) => {
+        // TODO Make this a proper error;
+        if(!hasProperArgType(token, commandParams, index))
+            throw new ASRuntimeError(`Token type doesn't match the required type`);
+        
+        if (token instanceof CommandToken) {
+            return runCommand(token, store);
+        }
+
+        return token.value;
     });
 
-    return cmdUnit.exec(...paramsCP);
+    return commandDef.exec(...paramsCP);
+}
+
+/**
+ * Utility function that checks if a token provided matches with the rules of Parameter.
+ * @param token The token to be checked
+ * @param params the Parameter object of a Command
+ * @param index the current index of the token
+ */
+function hasProperArgType(token: LangTokenBase<unknown>, params: Parameters, index: number) {
+    let type: DataType;
+
+    /** If the index is greater than param lengths, the assume the remaining types to be the last parameter type*/
+    if(index > params.getParamsLen - 1)
+        type = params.lastParam;
+    else
+        type = params.types[index];
+
+    if(type === DataType.any)
+        return true;
+
+    if(token instanceof NumberToken && type != DataType.number)
+        return false;
+
+    if(token instanceof BooleanToken && type != DataType.boolean)
+        return false;
+
+    if(token instanceof CommandToken && type != DataType.command)
+        return false;
+
+    return !(token instanceof StringToken && type != DataType.string);
 }
