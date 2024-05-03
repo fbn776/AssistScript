30d65ddeefd9716efbfc500bb156788bc1bb930e
fbn776 <fbn776@gmail.com>
2024-05-03 13:48:18 +0530

    fix: `CommandStore` now checks if the command names are valid

--- src/lang-core/utils/lang_utils.test.ts
+++ src/lang-core/utils/lang_utils.test.ts
@@ -1,6 +1,4 @@
-import {hasOnlyRepeatedChars} from "../../src/lang-core/utils/lang_utils";
-import {describe} from "node:test";
-
+import {hasOnlyRepeatedChars, isNameValid} from "./lang_utils";
 
 describe("hasOnlyRepeatedChars", () => {
     test('Checks if `aaaa` has repeated characters or not', () => {
@@ -9,4 +7,11 @@ describe("hasOnlyRepeatedChars", () => {
     test('Check if `aaab` has repeated character or not', () => {
         expect(hasOnlyRepeatedChars("aaab")).toBe(false);
     });
+});
+
+describe("isNameValid", () => {
+    test('Check if `hello` is a valid name', () => {
+        const x = isNameValid("hello")
+        expect(x).toBe(true);
+    })
 });
