0328f3ebb81453674cabcc7126f63b8dd75107d5
fbn776 <fbn776@gmail.com>
2024-05-10 21:46:44 +0530

    feat: while loop

--- src/lang-core/services/BaseContextProvider.ts
+++ src/lang-core/services/BaseContextProvider.ts
@@ -15,7 +15,12 @@ export default class BaseContextProvider {
     readonly stdout = console;
 
     /** No of times a loop can run */
-    readonly LOOP_LIMIT: number = 10;
+    readonly LOOP_LIMIT: number = 100;
+    /** Used for loop controls*/
+    isBreakCalled: boolean = false;
+    isContinueCalled: boolean = false;
+    /** Used to check if the interpreter is in a loop statement*/
+    isInLoop: boolean = false;
 
     /** A service that handles the storage of variables. */
     readonly storeService: VariableStore = new VariableStore();
@@ -23,6 +28,7 @@ export default class BaseContextProvider {
     /** Keep track of the current running command and the current root command*/
     private currCmd: CommandToken | null = null;
     private currState: T_InitialState | null = null;
+
     get currentCommand() {
         return this.currCmd;
     }
--- src/lang-core/stdlib/ctrl/loops.ts
+++ src/lang-core/stdlib/ctrl/loops.ts
@@ -2,13 +2,35 @@ import CommandStore from "../../interpreter/CommandStore";
 import {CommandBuilder} from "../../specs/CommandBuilder";
 import DataType from "../../specs/tokens/DataType";
 import {DocsBuilder} from "../../specs/DocsBuilder";
-import BaseContextProvider from "../../services/BaseContextProvider";
 import ASGracefulExitError from "../../errors/ASGracefulExitError";
+import ASRuntimeError from "../../errors/ASRuntimeError";
 
 const store = CommandStore.getInstance();
 const builder = new CommandBuilder();
 
-// TODO
+store.addCommand(builder
+    .names('break')
+    .args(0)
+    .docs(new DocsBuilder()
+        .name('break')
+        .description('Breaks out of the loop in which it is called.')
+        .syntax('break')
+        .example('(set i 0) (while (TRUE)) ((print (get i) (if (gt (get i) 5) (break))) (set i (add (get i) 1)))')
+        .build()
+    )
+    .returnType(DataType.void)
+    .run(_ => {
+        if(!_.isInLoop)
+            throw new ASRuntimeError('`break` can only be used inside a loop.', {
+                state: _.currentState!,
+                occurredCmd: _.currentCommand!,
+            })
+        _.isBreakCalled = true;
+    })
+    .build()
+)
+
+// WHILE
 store.addCommand(builder
     .names('while')
     .args(2, DataType.command)
@@ -16,21 +38,38 @@ store.addCommand(builder
         .name('while')
         .description('Loops while the condition is true.')
         .syntax('while <condition> <command>')
-        .example('TODO') // TODO
+        .example('while (TRUE) (p (get i))')
         .build()
     )
     .returnType(DataType.void)
     .run((_, condition: () => boolean, command: () => unknown) => {
-        const ctx = _ as BaseContextProvider;
+        console.log('while: condition:', condition())
 
         let limit = 0;
+        _.isInLoop = true;
         while (condition()) {
-            if(limit >= ctx.LOOP_LIMIT) {
-                throw new ASGracefulExitError(`Loop limit of ${ctx.LOOP_LIMIT} exceeded.`);
+            if (_.isBreakCalled) {
+                _.isBreakCalled = false;
+                break;
+            }
+
+            if(_.isContinueCalled) {
+                _.isContinueCalled = false;
+                continue;
+            }
+
+            console.log('limit', limit)
+
+            if ((limit++) >= _.LOOP_LIMIT) {
+                _.isInLoop = false;
+                throw new ASGracefulExitError(`Loop limit of ${_.LOOP_LIMIT} exceeded.`);
             }
 
             command();
         }
+        _.isInLoop = false;
     })
     .build()
 )
+
+
