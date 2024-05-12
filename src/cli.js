4744410f4947b5725cef4b930885b574ef6c374e
fbn776 <fbn776@gmail.com>
2024-05-12 22:21:41 +0530

    npm publish

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+let x = 10;
+let y = 20;
+
+console.log(x + y);
--- tsup.config.ts
+++ tsup.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
     splitting: false,
     sourcemap: true,
     clean: true,
-    minify: true,
+    // minify: true,
 });
