53baa23ba20966ffb7f35636ff6df8e8aff996ee
fbn776 <fbn776@gmail.com>
2024-05-16 22:53:54 +0530

    revert

--- src/errors/index.ts
+++
@@ -1,8 +0,0 @@
-import ASBaseError from "../lang-core/errors/ASBaseError";
-import ASRuntimeError from "../lang-core/errors/ASRuntimeError";
-import ASInterrupt from "../lang-core/errors/ASInterrupt";
-import ASMakeError from "../lang-core/errors/ASMakeError";
-import ASLangError from "../lang-core/errors/ASLangError";
-import ASGracefulExitError from "../lang-core/errors/ASGracefulExitError";
-
-export {ASBaseError, ASRuntimeError, ASLangError, ASMakeError, ASInterrupt, ASGracefulExitError}
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,22 @@ import CommandStore from "./lang-core/interpreter/CommandStore";
 import {CommandBuilder} from "./lang-core/specs/CommandBuilder";
 import {DocsBuilder} from "./lang-core/specs/DocsBuilder";
 import DataType from "./lang-core/specs/tokens/DataType";
-import * as Errors from './errors/index'
+import ASRuntimeError from "./lang-core/errors/ASRuntimeError";
+import ASLangError from "./lang-core/errors/ASLangError";
+import ASMakeError from "./lang-core/errors/ASMakeError";
+import ASInterrupt from "./lang-core/errors/ASInterrupt";
+import ASGracefulExitError from "./lang-core/errors/ASGracefulExitError";
+import ASBaseError from "./lang-core/errors/ASBaseError";
+
+const ASErrors = {
+    ASBaseError,
+    ASRuntimeError,
+    ASLangError,
+    ASMakeError,
+    ASInterrupt,
+    ASGracefulExitError
+}
+
 
 /** Other useful classes */
-export {AssistScript, BaseContextProvider, CommandStore, CommandBuilder, DocsBuilder, DataType, Errors};
+export {AssistScript, BaseContextProvider, CommandStore, CommandBuilder, DocsBuilder, DataType, ASErrors};
