1557998182a4f5226d58f6fdf6e4cdce71f1ac48
fbn776 <fbn776@gmail.com>
2024-05-16 22:15:19 +0530

    feat: The lib now exports `ASBaseError`

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,7 @@ import CommandStore from "./lang-core/interpreter/CommandStore";
 import {CommandBuilder} from "./lang-core/specs/CommandBuilder";
 import {DocsBuilder} from "./lang-core/specs/DocsBuilder";
 import DataType from "./lang-core/specs/tokens/DataType";
-
-/** The actual AssistScript class */
+import ASBaseError from "./lang-core/errors/ASBaseError";
 
 /** Other useful classes */
-export {AssistScript, BaseContextProvider, CommandStore, CommandBuilder, DocsBuilder, DataType};
+export {AssistScript, BaseContextProvider, CommandStore, CommandBuilder, DocsBuilder, DataType, ASBaseError};
